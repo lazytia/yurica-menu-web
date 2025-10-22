271b14b6a025aa8b9a6c8eeafa3cac9210bc2d47
lazytia <57658400+lazytia@users.noreply.github.com>
2025-10-22 15:49:44 +0900

    Update next.config.mjs to include output setting

--- admin/next.config.mjs
+++ admin/next.config.mjs
@@ -1,5 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { appDir: true },
-};
-export default nextConfig;
+ (cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF' 
+diff --git a/admin/next.config.mjs b/admin/next.config.mjs
+index 667b41501446bd2f8cc7119f66df116115c76576..bf221d35fdb91e610f05e63d105e926869c02148 100644
+--- a/admin/next.config.mjs
++++ b/admin/next.config.mjs
+@@ -1,5 +1,6 @@
+ /** @type {import('next').NextConfig} */
+ const nextConfig = {
+-  experimental: { appDir: true },
++  output: 'export',
+ };
++
+ export default nextConfig;
+ 
+EOF
+)
